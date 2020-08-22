53f44eec0da9b7fddbd0b76222931192860bc4d0
Ron Heywood <ron.heywood@spektrix.com>
2020-08-22 16:36:39 +0100

    adding blur

--- color-filtering.py
+++ color-filtering.py
@@ -16,5 +16,19 @@ cv2.imshow('original',launchMonitor)
 cv2.imshow('mask',mask)
 cv2.imshow('res',res)
 
+kernel = np.ones((15,15),np.float32)/225
+smoothed = cv2.filter2D(res,-1,kernel)
+
+cv2.imshow('Averaging',smoothed)
+
+blur = cv2.GaussianBlur(res,(15,15),0)
+cv2.imshow('Gaussian Blurring',blur)
+
+median = cv2.medianBlur(res,15)
+cv2.imshow('Median Blur',median)
+
+bilateral = cv2.bilateralFilter(res,15,75,75)
+cv2.imshow('bilateral Blur',bilateral)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
