15520983d9ce44582f2d000599ca15f0e033fe2b
Ron Heywood <ron.heywood@spektrix.com>
2021-03-11 08:23:29 +0000

    Canny ege detection looks like it might be able to find dimples

--- feature-detection.py
+++ feature-detection.py
@@ -25,6 +25,8 @@ if ball:
                         
     # you can generate kernels for embossing top as well
     gray = cv2.cvtColor(section, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Gray",gray)
+
     embossdepth = np.ones((h, w), np.uint8) * 128
     embossed = cv2.add(cv2.filter2D(gray, -1, kernel1),embossdepth) # emboss on bottom left side
     cv2.imshow("Embossed",embossed)
@@ -32,6 +34,24 @@ if ball:
     _, img_bin = cv2.threshold(cv2.cvtColor(section, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     img_bin = 255 - img_bin
     cv2.imshow("Binary",img_bin)
+
+    edges = cv2.Canny(gray,100,200)
+    cv2.imshow("Edges",edges)
+
+    embossed_edges = cv2.Canny(embossed,100,200)
+    cv2.imshow("Embossed Edges",embossed_edges)
+    
+    contours,heirarchy = cv2.findContours(embossed_edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    im2 = cv2.cvtColor(section, cv2.COLOR_BGR2GRAY)
+    dimples = []
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True)
+        area = cv2.contourArea(contour)
+        if ((len(approx) > 8) & (area > 30) ):
+            dimples.append(contour)
+
+    cv2.drawContours(im2, dimples, -1, (0,255,0), 3)
+    cv2.imshow("Contours",im2)
 
     cv2.rectangle(image, (x,y), (x_plus_w,y_plus_h), color, 2)
     cv2.putText(image, label, (x-10,y_plus_h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
