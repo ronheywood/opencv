2b234ab70686040f721371456c4e08c0f89fdc55
Ron Heywood <ron.heywood@spektrix.com>
2021-03-14 15:48:23 +0000

    Trying to use HoughCircles to find the center of the ball

--- detect-round-things.py
+++ detect-round-things.py
@@ -1,56 +1,74 @@
+# import the necessary packages
 import cv2
+import sys
+import os
 import numpy as np
+import argparse
+import time
+sys.path.append(os.path.abspath('./modules/'))
+import detection
+import helpers
 from matplotlib import pyplot as plt
 
-image = cv2.imread('test-images/at-rest.png',1)
-#image = cv2.imread('test-images/at-rest-teed-up.png',1) 
-#image = cv2.imread('test-images/high-spin-lob-wedge.png',1)
-#image = cv2.imread('test-images/close-up-outdoor--no-club-grass.png',1)
 
-output = image.copy()
-height, width = image.shape[:2]
-maxRadius = int(2.1*(width/12)/2)
-minRadius = int(0.9*(width/12)/2)
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--debug", type=str, 
+    default = None,
+    help="Show each mask detection for Debugging")
+ap.add_argument("-i", "--imagepath", type=str, 
+    default = None,
+    help="path to test image")
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#gray = cv2.GaussianBlur(gray,(5,5),0)
-frame_HSV = cv2.cvtColor(output, cv2.COLOR_BGR2HSV)
+args = ap.parse_args()
+if(args.imagepath is None):
+    image_path = helpers.get_random_test_image()
+else:
+    image_path = args.imagepath
 
-# Define range of white color in HSV
-lower_white = np.array([0, 0, 80])
-upper_white = np.array([131, 255, 255])
-# Threshold the HSV image
-mask = cv2.inRange(frame_HSV, lower_white, upper_white)
-ret,thresh = cv2.threshold(gray,127,255,cv2.THRESH_TOZERO)
-edges = cv2.Canny(thresh,100,200)
+image = cv2.imread(image_path,1)
+ball = detection.GolfBallDetection(image)
 
-plt.subplot(121),plt.imshow(thresh,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()
+if ball:
+    (x,y,w,h) = ball
+    output = image.copy()
+    #The boundaries are often not very accurate
+    #so we can expand on them a little to make the mask better
+    x -= 10
+    y -= 10
+    w += 30
+    h += 30
 
-# circles = cv2.HoughCircles(image=thresh, 
-#                            method=cv2.HOUGH_GRADIENT, 
-#                            dp=1.9, 
-#                            minDist=5,
-#                            param1=10,
-#                            param2=10,
-#                            minRadius=50,
-#                            maxRadius=0                           
-#                           )
-# circles = None
-# if circles is not None:
-#     # convert the (x, y) coordinates and radius of the circles to integers
-#     circlesRound = np.round(circles[0, :]).astype("int")
-#     # loop over the (x, y) coordinates and radius of the circles
-#     for (x, y, r) in circlesRound:
-#         cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+    x_plus_w = x+w
+    y_plus_h = y+h
 
-#     cv2.imshow('detected circles',output)
-# else:
-#     print ('No circles found')
-#     cv2.imshow('Copy',output)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)[y:y+h, x:x+w]
+    gray = cv2.GaussianBlur(gray,(5,5),0)
     
-cv2.waitKey(0)
+    circles = None
+    start = time.time()
+    # detect circles in the image
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1.9,minDist=w)
+    # ensure at least some circles were found
+    if circles is not None:
+        end = time.time()
+        print(f'[INFO] circle detection took {(end - start)} seconds')
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+        # loop over the (x, y) coordinates and radius of the circles
+        for (cx, cy, r) in circles:
+            cx +=x 
+            cy +=y
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circle
+            cv2.circle(output, (cx, cy), r, (0, 255, 0), 4)
+            cv2.rectangle(output, (cx - 5, cy - 5), (cx + 5, cy + 5), (0, 128, 255), -1)
+        # show the output image
+        cv2.imshow("Output", output)
+        
+    else:
+        print ('No circles found')
+        detection.draw_boundaries_and_label(output,(x,y),(w,h),(0,255,0),"No circle radius found")
+        cv2.imshow('Copy',output)
+
+cv2.waitKey(0)    
 cv2.destroyAllWindows()
